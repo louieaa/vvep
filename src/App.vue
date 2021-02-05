305d8b4dbf77ec0ff506defbc4b986c9f4164627
louieaa <42113246+louieaa@users.noreply.github.com>
2021-02-05 15:25:30 +0800

    New: 别名配置

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Com></Com>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Com from 'comps/Com.vue'
 </script>
 
 <style>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
-}
+  alias: {
+    '@': path.resolve(__dirname, 'src'),
+    comps: path.resolve(__dirname, 'src/components'),
+    styles: path.resolve(__dirname, 'src/styles'),
+    plugins: path.resolve(__dirname, 'src/plugins'),
+    views: path.resolve(__dirname, 'src/views'),
+    layouts: path.resolve(__dirname, 'src/layouts'),
+    utils: path.resolve(__dirname, 'src/utils'),
+    apis: path.resolve(__dirname, 'src/apis'),
+    dirs: path.resolve(__dirname, 'src/directives'),
+  },
+  plugins: [vue()],
+};
