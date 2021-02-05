80fb91fec240760f63f376aa61602401ef72c7e4
louieaa <42113246+louieaa@users.noreply.github.com>
2021-02-05 16:40:55 +0800

    New: 布局搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import Layout from 'layouts/index.vue'
 export const routes = [
+  // {
+  //   path: "/",
+  //   component:()=> import('views/home.vue')
+  // }
   {
     path: "/",
-    component:()=> import('views/home.vue')
+    component: Layout,
   }
 ];
 const router = createRouter({ history: createWebHashHistory(), routes });
